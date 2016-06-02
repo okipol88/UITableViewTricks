2728664b1a318efd1424e3ed4129c56d72a25ae7
Błażej Szajrych <blazej.szajrych@gmail.com>
2016-06-02 21:45:50 +0200

    Added framework

---
+++ CircleView/CircleView.h
@@ -0,0 +1,19 @@
+//
+//  CircleView.h
+//  CircleView
+//
+//  Created by Błażej Szajrych on 02.06.2016.
+//  Copyright © 2016 Integral Development Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircleView.
+FOUNDATION_EXPORT double CircleViewVersionNumber;
+
+//! Project version string for CircleView.
+FOUNDATION_EXPORT const unsigned char CircleViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircleView/PublicHeader.h>
+
+
